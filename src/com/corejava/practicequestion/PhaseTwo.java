d42f0cf1dd8ea3fabf9c3b751a33f0ddd1b42aac
thechaurasiya <iamkc0710@gmail.com>
2025-05-28 21:26:15 +0530

    Complete Practice sets

--- src/com/corejava/practicequestion/PhaseTwo.java
+++ src/com/corejava/practicequestion/PhaseTwo.java
@@ -3,5 +3,6 @@ package com.corejava.practicequestion;
 public class PhaseTwo {
     public static void main(String[] args) {
         System.out.println("Its main class");
+        System.out.println("makkkaaa djjd");
     }
 }
