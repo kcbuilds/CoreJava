5a591f1399a6514ae8fd0c45303180ee8411db47
Karan Chaurasiya <karan.chaurasiya@outlook.com>
2025-10-01 21:28:41 +0530

    Oct1

---
+++ src/com/corejava/multiThreading/ReadWriteCounter.java
@@ -0,0 +1,74 @@
+package com.corejava.multiThreading;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class ReadWriteCounter {
+
+    private final ReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
+
+    private int count = 0;
+
+    public void increment(){
+        writeLock.lock();
+        try {
+            Thread.sleep(50);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            count++;
+        }finally {
+            writeLock.unlock();
+        }
+    }
+
+    public int getCount() {
+        readLock.lock();
+        try {
+        return count;
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        ReadWriteCounter counter = new ReadWriteCounter();
+        
+        Runnable readTask = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(Thread.currentThread().getName() + " read " + counter.getCount());
+                }
+            }
+        };
+
+        Runnable writeTask = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    counter.increment();
+                    System.out.println(Thread.currentThread().getName() + " incremented " + counter.getCount() );
+                }
+            }
+        };
+
+        Thread writerThread = new Thread(writeTask);
+        Thread readerThread1 = new Thread(readTask);
+        Thread readerThread2 = new Thread(readTask);
+
+        writerThread.start();
+        readerThread1.start();
+        readerThread2.start();
+
+        writerThread.join();
+        readerThread1.join();
+        readerThread2.join();
+
+    }
+}
