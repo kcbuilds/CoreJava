71239bd8b9682494b5b7c514256f37329468dc92
kcbuilds <karan.chaurasiya@outlook.com>
2025-10-14 23:55:56 +0530

    Oct14

--- src/com/corejava/multiThreading/PracticesQues/Main.java
+++ src/com/corejava/multiThreading/PracticesQues/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         ThreadsTwo threadsTwo = new ThreadsTwo();
         ThreadsOne threadsOne = new ThreadsOne();
-        Thread t1 = new Thread(threadsOne);
+//        Thread t1 = new Thread(threadsOne);
 
 
 
