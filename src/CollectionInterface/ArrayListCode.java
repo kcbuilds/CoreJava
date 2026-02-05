d6a081b2044e2080c631e5235e1e7cf37b7a9327
kcbuilds <karan.chaurasiya@outlook.com>
2026-02-05 12:21:24 +0530

    05/02

---
+++ src/CollectionInterface/ArrayListCode.java
@@ -0,0 +1,77 @@
+package CollectionInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListCode {
+    public static void main(String[] args) {
+      /*  ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(20);
+        list.add(36);  // add element to array
+        System.out.println(list.get(2)); // To get value
+        System.out.println(list.size()); // Get a size
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+        for (int x : list){
+            System.out.println(x);
+        }
+
+        System.out.println(list.contains(5)); // Check if value present
+        System.out.println(list.contains(20));
+
+        list.remove(1); //remove element
+        for (int x : list){
+            System.out.println(x);
+        }
+
+        list.add(2,30); // add element anywhere without loose other
+        for (int x : list){
+            System.out.println(x);
+        }
+
+        list.set(1,500); // add element with replace that element
+        for (int x : list){
+            System.out.println(x);
+        }
+
+        ArrayList<Integer> listOne = new ArrayList<>(50);
+        System.out.println(listOne.size());
+        listOne.add(1);
+        System.out.println(listOne.size());
+
+        List<String> list1 = Arrays.asList("Monday", "Tuesday");
+        list1.set(1,"Wednesday");  // Only replace allow
+
+        List<String> list4 = new ArrayList<>(list1);
+        list4.add("Thursday");
+        System.out.println(list4   );
+
+        Integer[] array = {2,4,3};
+        List<Integer> list2 = Arrays.asList(array);
+        list2.set(1,50);
+        System.out.println(list2.get(1));
+
+
+
+        List<Integer> integers = List.of(1, 2, 3);
+//      integers.set(1,50); --- set/add not allow
+
+
+       */
+
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Amit");
+        names.add("Ravi");
+        names.add("Neha");
+
+        names.remove("Ravi");
+        System.out.println(names);
+
+
+    }
+}
---
+++ src/CollectionInterface/ComparatorCode.java
@@ -0,0 +1,99 @@
+package CollectionInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+class MyComparator implements Comparator<Integer> {
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2 - o1; // Descending Order
+        // return o1 - o2; // Ascending Order
+
+        /*
+          If 5-3=2 -- Positive- o1 come first & o2 second
+          If 3-5=-2 -- Negative- o2 come first & o1 second
+          if 0 -- Both have same
+        */
+    }
+}
+
+class MyComparator2 implements Comparator<String>{
+
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length() - o2 .length();
+    }
+}
+
+class Student{
+    private String name;
+    private double gpa;
+
+    public Student(String name, double gpa){
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+}
+
+
+
+public class ComparatorCode {
+    public static void main(String[] args) {
+        List<Integer> num = new ArrayList<>();
+        num.add(5);
+        num.add(5);
+        num.add(2);
+        num.add(5);
+
+        System.out.println(num);
+
+        num.sort(new MyComparator());
+        System.out.println(num);
+
+        List<String> list = Arrays.asList("Monday", "Auesday", "Wednesday");
+        list.sort(new MyComparator2());
+        System.out.println(list);
+
+
+        List<String> list1 = Arrays.asList("Applef","Egg","Orange","Mango");
+        list1.sort((a,b) -> b.length()-a.length());
+        System.out.println(list1);
+
+        List<Student> student = new ArrayList<>();
+        student.add(new Student("Karan", 7.0));
+        student.add(new Student("Deepak", 5.0));
+        student.add(new Student("Prem", 7.5));
+        student.add(new Student("Viru", 6.3));
+//        student.sort((o1, o2) ->{
+//            if (o2.getGpa() - o1.getGpa() > 0) {
+//                return 1;
+//            } else if (o2.getGpa() - o1.getGpa() < 0) {
+//                return -1;
+//            }else {
+//                return o1.getName().compareTo(o2.getName());
+//            }
+//        });
+        
+        //Using Java 8 Features
+
+        Comparator<Student> comparing = Comparator.comparing(Student::getGpa).reversed().thenComparing(Student::getName);
+
+        student.sort(comparing);
+        for (Student s: student){
+            System.out.println(s.getName() + ": " +s.getGpa());
+        }
+
+
+    }
+}
