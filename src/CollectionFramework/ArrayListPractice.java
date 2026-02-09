f11ce4ea189c9468e4e9a141702777ef6819eac1
kcbuilds <karan.chaurasiya@outlook.com>
2026-02-09 12:42:18 +0530

    09/02

---
+++ src/CollectionFramework/ArrayListPractice.java
@@ -0,0 +1,142 @@
+package CollectionFramework;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+class Employee{
+    String name;
+    int salary;
+
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return name + " : " + salary;
+    }
+}
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        List<Integer> studentMarks = new ArrayList<>();
+
+        studentMarks.add(45);
+        studentMarks.add(90);
+        studentMarks.add(12);
+        studentMarks.add(67);
+        studentMarks.add(34);
+        System.out.println(studentMarks);
+
+        studentMarks.remove(2);
+        System.out.println(studentMarks);
+
+        System.out.println(studentMarks.get(2));
+
+        for (int marks : studentMarks){
+            System.out.print(marks +" ");
+        }
+        System.out.println();
+
+//        studentMarks.sort(null);
+//        System.out.println(studentMarks);
+
+        Collections.sort(studentMarks, (a,b) -> a - b);
+        System.out.println(studentMarks);
+
+        Collections.sort(studentMarks, (a,b) -> b-a);
+        System.out.println(studentMarks);
+
+        List<Employee> employees = new ArrayList<>();
+        employees.add(new Employee("Deepak",30000));
+        employees.add(new Employee("Karan",50000));
+        employees.add(new Employee("Ritesh",40000));
+        employees.add(new Employee("Vishwajeet",50000));
+
+
+
+        if (employees.isEmpty()){
+            System.out.println("No Data");
+        } else {
+            employees.sort(Comparator.comparing((Employee e) -> e.salary).thenComparing(e -> e.name).reversed());
+            for (Employee e : employees){
+                System.out.println(e);
+            }
+        }
+
+        // Student Ranking System
+
+        ArrayList<StudentRankingSystem> studentRankingSystems = new ArrayList<>();
+        studentRankingSystems.add(new StudentRankingSystem("Karan",95,24));
+        studentRankingSystems.add(new StudentRankingSystem("Deepak",80,23));
+        studentRankingSystems.add(new StudentRankingSystem("Prem",70,24));
+        studentRankingSystems.add(new StudentRankingSystem("Amar",95,23));
+
+        if (studentRankingSystems.isEmpty()){
+            System.out.println("No Data Available");
+        } else {
+            studentRankingSystems.sort(
+                    // Less Prof..
+//                  Comparator.comparing((StudentRankingSystem s) -> s.getMarks())
+//                          .reversed()
+//                          .thenComparing(s -> s.getAge()));
+
+                    // More Prof...
+                    Comparator.comparing(StudentRankingSystem :: getMarks)
+                            .reversed()
+                            .thenComparing(StudentRankingSystem::getAge));
+
+        }
+
+        for (StudentRankingSystem system: studentRankingSystems){
+            System.out.println(system);
+        }
+
+
+
+    }
+
+    static class StudentRankingSystem{
+        private String name;
+        private int marks;
+        private int age;
+
+        public StudentRankingSystem(String name, int marks, int age) {
+            this.name = name;
+            this.marks = marks;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getMarks() {
+            return marks;
+        }
+
+        public void setMarks(int marks) {
+            this.marks = marks;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Student Name: " +name +"," +" Age: " +age +"," +" Marks: " +marks ;
+        }
+    }
+}
--- src/Map/HashmapCode.java
+++ src/Map/HashmapCode.java
@@ -2,40 +2,113 @@ package Map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
+
+class Student{
+    private int ID;
+    private String name;
+
+    public Student(int id, String name){
+        this.ID = id;
+        this.name = name;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ID, name );
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj == null){
+            return false;
+        }
+        if (getClass() != obj.getClass()){
+            return false;
+        }
+        Student other = (Student) obj;
+        return ID == other.getID() && Objects.equals(name, other.getName());
+    }
+}
+
 
 public class HashmapCode {
     public static void main(String[] args) {
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(01, "Karan");
-        map.put(05, "Deepak");
-        map.put(03, "Prem");
+//        HashMap<Integer, String> map = new HashMap<>();
+//        map.put(01, "Karan");
+//        map.put(05, "Deepak");
+//        map.put(03, "Prem");
+//
+//        System.out.println(map);
+//
+//        System.out.println(map.get(01));
+//        System.out.println(map.containsKey(03));
+//        System.out.println(map.containsValue("Prem"));
+//
+//        for (int i : map.keySet()){
+//            System.out.println(map.get(i));
+//        }
+//
+//        Set<Map.Entry<Integer, String>> entries = map.entrySet();
+//        for (Map.Entry<Integer, String> entry : entries){
+//          System.out.println(entry.getKey() +" : "+ entry.getValue());
+//            entry.setValue(entry.getValue().toUpperCase());
+//        }
+//        System.out.println(map);
+//
+//        map.put(null, "Viru");
+//        map.put(1, "Om"); // Replace Key 1 value
+//        System.out.println(map);
+//
+//    map.remove(01);
+//        boolean res = map.remove(01, "Prem"); //check both before remove
+//        System.out.println(res);
+//        System.out.println(map);
 
-        System.out.println(map);
+        HashMap<Student,Integer> hashMap = new HashMap<>();
+        Student s1 = new Student(01,"Deepak G.");
+        Student s2 = new Student(02,"Karan C.");
+        Student s3 = new Student(01,"Deepak G."); // Store in different Memory
 
-        System.out.println(map.get(01));
-        System.out.println(map.containsKey(03));
-        System.out.println(map.containsValue("Prem"));
+        hashMap.put(s1,90); // Hashcode1 --> index1
+        hashMap.put(s2,70); // Hashcode2 --> index2
+        hashMap.put(s3,85); // Hashcode3 --> index3
 
-        for (int i : map.keySet()){
-            System.out.println(map.get(i));
-        }
+        System.out.println(hashMap.size());
+        System.out.println(hashMap.get(s1));
+        System.out.println(hashMap.get(s3));
+//        System.out.println(s1);
 
-        Set<Map.Entry<Integer, String>> entries = map.entrySet();
-        for (Map.Entry<Integer, String> entry : entries){
-//            System.out.println(entry.getKey() +" : "+ entry.getValue());
-            entry.setValue(entry.getValue().toUpperCase());
-        }
-        System.out.println(map);
 
-        map.put(null, "Viru");
-        map.put(1, "Om"); // Replace Key 1 value
-        System.out.println(map);
+        HashMap<String, Integer> hashMap1 = new HashMap<>();
+        hashMap1.put("Deepak", 90); // Hashcode1 --> index1
+        hashMap1.put("Prem", 80); // Hashcode2 --> index2
+        hashMap1.put("Deepak", 95); // Hashcode1 --> index1 --> equals() --> replace
 
-//        map.remove(01);
-        boolean res = map.remove(01, "Prem"); //check both before remove
-        System.out.println(res);
-        System.out.println(map);
+        System.out.println(hashMap1.size());
+        System.out.println(hashMap1.get("Deepak"));
+        System.out.println(hashMap1.get("Deepak"));
 
     }
 }
